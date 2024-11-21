400405d5e479ae0110f7144a98fd9ae597ad3a0d
Walassi Silva <walassi.b@gmail.com>
2024-11-21 18:13:53 -0300

    feat: finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
+import H1 from "@/components/H1";
+import SearchForm from "@/components/SearchForm";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-36 px-3">
-      <h1 className="text-3xl font-bold tracking-tight lg:text-6xl">
-        Find events around you
-      </h1>
+      <H1>Find events around you</H1>
+
       <p className="mb-12 mt-7 text-2xl opacity-75 lg:text-3xl">
         Browse more than{" "}
-        <span className="font-bold italic text-[#a4f839]">10,000 events</span>{" "}
+        <span className="font-bold italic text-accent">10,000 events</span>{" "}
         around you
       </p>
-      <form action="" className="w-full sm:w-[580px]">
-        <input
-          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50  focus:ring-2 focus:bg-white/10 transition"
-          type="text"
-          placeholder="Search events in any city"
-          spellCheck="false"
-        />
-      </form>
+
+      <SearchForm />
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular: </p>
         <div className="space-x-2 font-semibold transition">
